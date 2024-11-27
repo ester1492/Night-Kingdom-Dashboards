61a7638889032f72d14c6452f976a29fa37d3b6e
Ester S. <ester.jobs.1492@gmail.com>
2024-11-27 23:16:00 +0200

    dasboards with images and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MatehNetzivut from './components/MatehNetzivut';
+import Machoz from './components/Machoz';
+import Bisar from './components/Bisar';
+import Soher from './components/Soher';
+import './styles.css'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1>hellow</h1>
-    </>
-  )
-}
+    // <h1>Hi!</h1>
+    <Router>
+      <Routes basename='/Night-Kingdom-Dashboards/'>
+        <Route path="/" element={<MatehNetzivut />} />
+        <Route path="/Machoz" element={<Machoz />} />
+        <Route path="/Bisar" element={<Bisar />} />
+        <Route path="/Soher" element={<Soher />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//      <h1>hellow</h1>
+//     </>
+//   )
+// }
+
+// export default App
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Night-Kingdom-IPS/',
+  base: '/Night-Kingdom-Dashboards/',
   resolve: {
     extensions: ['.js', '.jsx']
   },
