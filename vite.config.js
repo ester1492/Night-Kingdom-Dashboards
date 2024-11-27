a008d0fae53bc20056ec7b98f930af53e0fce1bb
Ruty-Pe <rutyperetz@gmail.com>
2024-11-27 18:13:01 +0200

    vit config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Night-Kingdom-IPS/'
+  base: '/Night-Kingdom-IPS/',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html', 
+    },
+  },
 });
