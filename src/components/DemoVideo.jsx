182a064ce6327b455644602c2a2644a8a66da840
Ester S. <ester.jobs.1492@gmail.com>
2024-11-28 15:51:36 +0200

    add link for video

--- src/components/DemoVideo.jsx
+++ src/components/DemoVideo.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const DemoVideo = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="page page5">
+            <button className="button button4" onClick={() => navigate('/')}>
+            </button>
+
             <div className="video-container">
                 <video controls autoPlay muted loop>
                     <source src="assets/demoVideo.mp4" type="video/mp4" />
--- src/components/MatehNetzivut.jsx
+++ src/components/MatehNetzivut.jsx
@@ -5,7 +5,11 @@ const MatehNetzivut = () => {
   const navigate = useNavigate();
   return (
     <div className="page page1">
-            <button className="button button4" onClick={() => navigate('/')}>
+      <button className="button videoLink" onClick={() => navigate('/demoVideo')}>
+      </button>
+
+
+      <button className="button button4" onClick={() => navigate('/')}>
       </button>
 
       <button className="button button1" onClick={() => navigate('/Machoz')}>
