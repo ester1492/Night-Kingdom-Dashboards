21754a30eee3bb97e6dcf7b39a0137756c3f16fe
Ester S. <ester.jobs.1492@gmail.com>
2024-11-28 16:15:33 +0200

    add video icon

--- src/components/MatehNetzivut.jsx
+++ src/components/MatehNetzivut.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import  {OndemandVideo} from '@mui/icons-material'
 const MatehNetzivut = () => {
   const navigate = useNavigate();
   return (
     <div className="page page1">
+
       <button className="button videoLink" onClick={() => navigate('/demoVideo')}>
+      
+      <OndemandVideo color='white'></OndemandVideo>
       </button>
 
 
