81a4f5d511055caf5d81a70571bf1978878306e3
Ester S. <ester.jobs.1492@gmail.com>
2024-11-28 10:00:58 +0200

    fix basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import MatehNetzivut from './components/MatehNetzivut';
 import Machoz from './components/Machoz';
 import Bisar from './components/Bisar';
@@ -8,15 +8,15 @@ import './styles.css'
 function App() {
   return (
     // <h1>Hi!</h1>
-    <MatehNetzivut />
-    // <Router>
-    //   <Routes basename='/Night-Kingdom-Dashboards/'>
-    //     <Route path="/" element={<MatehNetzivut />} />
-    //     <Route path="/Machoz" element={<Machoz />} />
-    //     <Route path="/Bisar" element={<Bisar />} />
-    //     <Route path="/Soher" element={<Soher />} />
-    //   </Routes>
-    // </Router>
+    
+    <BrowserRouter basename='/Night-Kingdom-Dashboards/'>
+      <Routes >
+        <Route path="/" element={<MatehNetzivut />} />
+        <Route path="/Machoz" element={<Machoz />} />
+        <Route path="/Bisar" element={<Bisar />} />
+        <Route path="/Soher" element={<Soher />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/components/MatehNetzivut.jsx
+++ src/components/MatehNetzivut.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const MatehNetzivut = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="page page1">
-      <button className="button button1">
-         {/* onClick={() => navigate('/Machoz')}> */}
+      <button className="button button1" onClick={() => navigate('/Machoz')}>
         Next
       </button>
     </div>
